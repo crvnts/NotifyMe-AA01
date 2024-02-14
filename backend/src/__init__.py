921cc015a87158b4f6ed2bee95868fb4a535c370
rjeong1530 <ryanj1534@gmail.com>
2024-02-14 18:57:04 -0500

    implemented flask + flaskpymongo

---
+++ backend/src/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb+srv://ryanj1534:Fe5uemCVwY8Rq1no@testaa01.2rnejhm.mongodb.net/Backend"
+# mongodb database
+mongo = PyMongo(app)
+db = mongo.db
+
+from src import mongodb
--- backend/src/mongodb.py
+++ backend/src/mongodb.py
@@ -1,26 +1,55 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-import os
-from dotenv import load_dotenv
+from src import app
+from flask import render_template, request, redirect, flash, url_for, Response
+from bson import ObjectId, json_util
+import json
 
-# Load variables from .env file
-load_dotenv()
-uri = os.getenv("MongoDBDriver")
+from src import db
+from datetime import datetime
 
-
-
-def mongo_connection():
-    # Create a new client and connect to the server
-    client = MongoClient(uri)
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-        return client['Backend']
-    except Exception as e:
-        print(e)
         
+@app.route("/register", methods ={'POST'})
+def insert_user():
+    request_data = request.get_json()
+    if request.method == "POST":
+        name = request_data['name']
+        email = request_data['email']
+        username = request_data['username']
+        password = request_data['password']
+        street = request_data['street']
+        city = request_data['city']
+        province = request_data['province']
+        postal = request_data['postal']
     
-def insert_user(connection, user):
-    collection_name = connection["users"]
-    collection_name.insert_one(user)
+    newUser ={
+        "name": name,       
+        "email": email,
+        "username": username,
+        "password": password,
+        "address": {
+            "street":street,
+            "city":city,
+            "province":province,
+            "postal":postal
+        }
+}    
+    try: 
+        db.users.insert_one(newUser)
+        return Response("User added successfully", status = 201)
+    except Exception as e:
+        print("An exception has occurred ::",e)
+        return False
+    
+@app.route("/login",methods = {'POST'})
+def login():
+    request_data = request.get_json()
+    if request.method == "POST":
+        username = request_data['username']
+        password = request_data['password']
+    
+    try:
+        user = db.users.find_one({"username":username})
+        #return Response(json.loads(json_util.dumps(user)),status=201)
+    except Exception as e:
+        print("error", e)
+    
+    
