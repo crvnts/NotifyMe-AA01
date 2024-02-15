a3637aa4b87cbf7b8c3a59c4572488ac6b10f573
Tom C <crvntst@gmail.com>
2024-02-15 14:50:14 -0500

    Properly structured frontend with routing

---
+++ frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React from "react";
+//import Navbar from 'navbar/Navbar';
+import "./Dashboard.css";
+
+function Dashboard() {
+  /*
+    useEffect( () => {
+        $(window).on('load', function(){
+            setTimeout(function() {
+                $('.dash-body').fadeIn('slow');
+            }, 750);
+        });
+        return () => {
+            console.log("unmounted");
+        };
+    }, []);
+    */
+
+  return (
+    <div className="background">
+      <div className="dash-body">
+        <div className="dash-side-menu">
+          <div className="dash-container">
+            <div className="dash-smenu-text">NotifyMe</div>
+          </div>
+        </div>
+        <div className="widget-top-row">
+          <div className="plan-trip-widget">
+            <div className="widget-text">Plan a Trip</div>
+          </div>
+          <div className="weather-widget">
+            <div className="widget-text">Headed out?</div>
+          </div>
+          <div className="ad-widget">
+            <div className="widget-text">Uber Eatz Ad</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
