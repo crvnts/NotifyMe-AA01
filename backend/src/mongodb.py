bc175576d6c2de52e7b916b7efc4c8fadc4c51c7
rjeong1530 <ryanj1534@gmail.com>
2024-02-14 21:24:09 -0500

    added hashing for passwords
    need to add jwtokens for auth

--- backend/src/mongodb.py
+++ backend/src/mongodb.py
@@ -2,11 +2,16 @@ from src import app
 from flask import render_template, request, redirect, flash, url_for, Response
 from bson import ObjectId, json_util
 import json
-
+import bcrypt
 from src import db
 from datetime import datetime
 
-        
+def get_hashed_password(plain_password):
+    return bcrypt.hashpw(plain_password.encode('utf-8'), bcrypt.gensalt())
+
+def check_password(plain_password, hashed_password):
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))   
+
 @app.route("/register", methods ={'POST'})
 def insert_user():
     request_data = request.get_json()
@@ -24,7 +29,7 @@ def insert_user():
         "name": name,       
         "email": email,
         "username": username,
-        "password": password,
+        "password": get_hashed_password(password).decode('utf-8'),
         "address": {
             "street":street,
             "city":city,
@@ -45,11 +50,12 @@ def login():
     if request.method == "POST":
         username = request_data['username']
         password = request_data['password']
-    
     try:
         user = db.users.find_one({"username":username})
         #return Response(json.loads(json_util.dumps(user)),status=201)
     except Exception as e:
         print("error", e)
     
+    if (check_password(password, user['password'])):
+        return Response("Logged into " + user['username'], status =201 )
     
