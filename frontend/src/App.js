358fa47ea756c785f0713c4eaedabe5525f1bc33
tjishere <thomasjo2001@gmail.com>
2024-02-22 19:25:47 -0500

    Page routing format matches main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ReactDOM } from 'react';
 import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 
-const rootElement = document.getElementById("root");
+//const rootElement = document.getElementById("root");
 const App = () => {
   return (
-<Router>
-  <Routes>
-    <Route exact path="/Dashboard" element={<Dashboard/>}/>
-    <Route exact path="/Login" element={<Login/>}/>
-  </Routes>
-  </Router>
+    <Router>
+      <Fragment>
+        <Routes>
+          <Route exact path="/Dashboard" element={<Dashboard/>}/>
+          <Route exact path="/Login" element={<Login/>}/>
+        </Routes>
+      </Fragment>
+    </Router>
   );
 };
 export default App;
